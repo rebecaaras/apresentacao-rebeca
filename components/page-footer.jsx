147f7fae3e2b34615bd2e9db213beaed20d349c0
rebecaaras <rebecaaraujo2013@gmail.com>
2024-10-31 19:45:48 -0300

    Add PageFooter component

---
+++ components/page-footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const PageFooter = () => {
+
+    return(
+        <>
+            <footer className="bg-slate-100 text-orange-950 rounded-[10px] p-2 h-10 w-30 text-opacity-60 p-[15px] mb-[15px]">
+                <a href="https://github.com/rebecaaras" target="_blank" rel="noopener noreferrer">Visite a página do projeto no GitHub!
+                <i className="fab fa-github" style={{ fontSize: "22px", color: "#43140790" }}></i></a>
+            </footer>
+        </>
+    )
+}
+
+export default PageFooter;
