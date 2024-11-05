1ebad88db6a0a21ea64e40151f16d4b05619da4f
rebecaaras <rebecaaraujo2013@gmail.com>
2024-11-05 16:06:03 -0300

    new: Add Home button

--- components/nav-bar.jsx
+++ components/nav-bar.jsx
@@ -8,6 +8,9 @@ const Navbar = () => {
             <nav className="text-orange-950 p-4 flex flex-row items-center">
                 <Image src="/logo.png" width="80" height="50"></Image>
                 <ul className="flex flex-row">
+                    <Link href='/' legacyBehavior passHref>
+                        <li className="bg-slate-100 text-orange-950 rounded-[10px] p-2 h-10 w-30 hover:bg-orange-950 hover:text-orange-950 text-opacity-60 hover:font-bold hover:bg-opacity-10">Home</li>
+                    </Link>
                     <Link href='/classic' legacyBehavior passHref>
                         <li className="bg-slate-100 text-orange-950 rounded-[10px] p-2 h-10 w-30 hover:bg-orange-950 hover:text-orange-950 text-opacity-60 hover:font-bold hover:bg-opacity-10">Clássicos</li>
                     </Link>
