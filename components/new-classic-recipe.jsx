7b90cbe5dc8274c21071839db719f57255766439
rebecaaras <rebecaaraujo2013@gmail.com>
2024-11-06 11:03:34 -0300

    new: Add sort random recipe to be displayed in Classic and Home pages

--- components/new-classic-recipe.jsx
+++ components/new-classic-recipe.jsx
@@ -1,10 +1,21 @@
+"use client";
+
 import React from "react";
 import { useEffect, useState } from 'react';
 
+const defaultRecipe = {
+    "nome": "Flat White",
+    "ingredientes": [
+      "leite vaporizado",
+      "café expresso"
+    ],
+    "instrucoes": "Prepare um expresso e adicione leite vaporizado, visando uma proporção maior de café na mistura."
+  }
 const NewClassicRecipe = () => {
     const [data, setData] = useState(null);
+    const [recipe, setRecipe] = useState(defaultRecipe);
 
-    const getNewClassicRecipe = async() => {
+    const getAllRecipes = async() => {
         fetch('/api/classic')
             .then(response => response.json())
             .then(data => setData(data))
@@ -12,8 +23,23 @@ const NewClassicRecipe = () => {
       };
 
     useEffect(() => {
-        getNewClassicRecipe();
+        getAllRecipes();
     }, []);
+
+    async function sortRecipe(){
+        let index = Math.floor(Math.random() * data.length);
+        setRecipe(data[index])
+        console.log('Recipe:', recipe)
+        console.log('Index:',index)
+        console.log('data[index]:', data[index])
+    }
+
+    useEffect(() => {
+        if (data){
+            sortRecipe();
+            console.log('Segundo useEffect executado!');
+        }
+    }, [data]);
 
     if (!data) {
         return (<div className="w-full flex flex-col items-center">
@@ -27,24 +53,24 @@ const NewClassicRecipe = () => {
             <header className="w-full bg-slate-100 text-white text-center pb-[15px]">
             <h1 className="font-[Roboto] text-[26px] text-orange-950">Descubra uma receita clássica:</h1>
             </header>
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center m-[15px]">
             <div className="max-w-4x1 bg-white p-4 rounded-lg shadow-md border-[2px] border-orange-950 border-opacity-80 mb-[15px]">
                 <h2 className= "font-[Roboto] text-orange-950 text-[22px] font-semibold">
-                    {data[0].nome}
+                    {recipe.nome}
                 </h2>
                 <ul className= "font-[Roboto] text-orange-950 flex flex-row">
                     <span className= "font-[Roboto] text-orange-950 font-semibold">Ingredientes:</span>
-                    {data[0].ingredientes.map(ingrediente =>(
+                    {recipe.ingredientes.map(ingrediente =>(
                         <li>&nbsp;{ingrediente},</li>
                     ))}
                 </ul>
                 <p className= "font-[Roboto] text-orange-950">
                 <span className= "font-[Roboto] text-orange-950 font-semibold">Instruções:&nbsp;</span>
-                    {data[0].instrucoes}
+                    {recipe.instrucoes}
                 </p>
             </div>
             <div className="p-1">
-                <button onClick={getNewClassicRecipe} className="bg-orange-950 bg-opacity-80 text-white font-[Roboto] rounded-[10px] p-2 h-12 w-30 hover:bg-orange-950 hover:bg-opacity-100 hover:font-bold border-[1px]">
+                <button onClick={getAllRecipes} className="bg-orange-950 bg-opacity-80 text-white font-[Roboto] rounded-[10px] p-2 h-12 w-30 hover:bg-orange-950 hover:bg-opacity-100 hover:font-bold border-[1px]">
                     Nova receita
                 </button>
             </div>
