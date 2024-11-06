142fe04cb53f55c807ccb2844059fcc7a6f3e3f7
rebecaaras <rebecaaraujo2013@gmail.com>
2024-11-06 16:44:38 -0300

    style: Adapt nav and footer to small screens

--- components/nav-bar.jsx
+++ components/nav-bar.jsx
@@ -5,20 +5,20 @@ import Link from "next/link";
 const Navbar = () => {
     return (
         <>
-            <nav className="text-orange-950 p-4 flex flex-row items-center">
-                <Image src="/logo.png" width="80" height="50"></Image>
+            <nav className="text-orange-950 p-4 flex flex-row items-center max-w-96">
+                <Image src="/logo.png" width="50" height="50"></Image>
                 <ul className="flex flex-row">
                     <Link href='/' legacyBehavior passHref>
-                        <li className="cursor-pointer bg-slate-100 text-orange-950 rounded-[10px] p-2 h-10 w-30 hover:bg-orange-950 hover:text-orange-950 text-opacity-60 hover:font-bold hover:bg-opacity-10">Home</li> 
+                        <li className="text-[19px] cursor-pointer bg-slate-100 text-orange-950 rounded-[10px] p-2 h-10 w-30 hover:bg-orange-950 hover:text-orange-950 text-opacity-60 hover:font-bold hover:bg-opacity-10">Home</li> 
                     </Link>
                     <Link href='/classic' legacyBehavior passHref>
-                        <li className="cursor-pointer bg-slate-100 text-orange-950 rounded-[10px] p-2 h-10 w-30 hover:bg-orange-950 hover:text-orange-950 text-opacity-60 hover:font-bold hover:bg-opacity-10">Clássicos</li>
+                        <li className="text-[19px] cursor-pointer bg-slate-100 text-orange-950 rounded-[10px] p-2 h-10 w-30 hover:bg-orange-950 hover:text-orange-950 text-opacity-60 hover:font-bold hover:bg-opacity-10">Clássicos</li>
                     </Link>
                     <Link href='/special' legacyBehaviour passHref>
-                        <li className="cursor-pointer bg-slate-100 text-orange-950 rounded-[10px] p-2 h-10 w-30 hover:bg-orange-950 hover:text-orange-950 text-opacity-60 hover:font-bold hover:bg-opacity-10">Especiais</li>
+                        <li className="text-[19px] cursor-pointer bg-slate-100 text-orange-950 rounded-[10px] p-2 h-10 w-30 hover:bg-orange-950 hover:text-orange-950 text-opacity-60 hover:font-bold hover:bg-opacity-10">Especiais</li>
                     </Link>
                     <Link href='/gourmet' legacyBehavior passHref>
-                        <li className="cursor-pointer bg-slate-100 text-orange-950 rounded-[10px] p-2 h-10 w-30 hover:bg-orange-950 hover:text-orange-950 text-opacity-60 hover:font-bold hover:bg-opacity-10">Gourmet</li>
+                        <li className="text-[19px] cursor-pointer bg-slate-100 text-orange-950 rounded-[10px] p-2 h-10 w-30 hover:bg-orange-950 hover:text-orange-950 text-opacity-60 hover:font-bold hover:bg-opacity-10">Gourmet</li>
                     </Link>
                 </ul>
             </nav>
